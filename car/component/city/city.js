3891578ec949224d8881e757c577dc9f32ee743b
Bonnie <user_reg@qq.com>
2018-06-13 23:19:33 +0800

    修改

---
+++ car/component/city/city.js
@@ -0,0 +1,110 @@
+import qqmap from '../../utils/map.js';
+Component({
+    properties: {
+        styles: {//这个是可以自定义最外层的view的样式
+            type: String,
+            value: '',
+            observer: function (newval, oldval) {
+                // 监听改变
+                console.log(newval, oldval);
+            }
+        },
+        citylist:Object
+    },
+    data: {
+        //下面是字母排序
+        letter: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+        cityListId: '',
+        coverState: false,
+        //下面是热门城市数据，模拟数据
+        newcity: ['北京', '上海', '广州', '深圳', '成都', '杭州'],
+        locateCity: ''
+    },
+    methods: {
+        //点击城市
+        cityTap(e) {
+            const val = e.currentTarget.dataset.val || '',
+                types = e.currentTarget.dataset.types || '',
+                Index = e.currentTarget.dataset.index || '',
+                that = this;
+            let city = '';           
+            switch (types) {
+                case 'locate':
+                    //定位内容
+                    city = this.data.locateCity;
+                    break;
+                case 'national':
+                    //全国
+                    city = '全国';
+                    break;
+                case 'new':
+                    //热门城市
+                    city = val;
+                    break;
+                case 'list':
+                    //城市列表
+                    city = val.cityName;
+                    break;
+            }
+            if (city) {
+                wx.setStorage({
+                    key: 'city',
+                    data: city
+                })
+                //点击后给父组件可以通过bindcitytap事件，获取到cityname的值，这是子组件给父组件传值和触发事件的方法
+                this.setData({
+                    locateCity:city
+                })
+                this.triggerEvent('citytap', { cityname: city });
+            } else {
+                console.log('还没有');
+                this.getLocate();
+            }
+
+        },
+        //点击城市字母
+        letterTap(e) {
+            const Item = e.currentTarget.dataset.item;
+            this.setData({
+                cityListId: Item,
+                coverState: true
+            });
+            setTimeout(() => {
+                this.setData({
+                    coverState: false
+                })
+            }, 600)
+        },
+        //调用定位
+        getLocate() {
+            new qqmap().getLocateInfo().then(val=>{//这个方法在另一个文件里，下面有贴出代码
+                if (val.indexOf('市') !== -1) {//这里是去掉“市”这个字
+                    val = val.slice(0, val.indexOf('市'));
+                }
+                this.setData({
+                    locateCity: val
+                });
+                //把获取的定位和获取的时间放到本地存储
+                wx.setStorageSync('locatecity', { city: val, time: new Date().getTime() });
+            });
+        },
+        //关闭弹框
+        closeTap() {
+            this.triggerEvent('closeCity');
+        }
+    },
+    ready() {
+        console.log(wx.getStorageSync('locatecity'))
+        let cityOrTime = wx.getStorageSync('locatecity') || {},
+            time = new Date().getTime(),
+            city = '';
+        if (!cityOrTime.time || (time - cityOrTime.time > 1800000)) {//每隔30分钟请求一次定位
+            this.getLocate();
+        } else {//如果未满30分钟，那么直接从本地缓存里取值
+            this.setData({
+                locateCity: cityOrTime.city
+            })
+        }
+    },
+    
+})
--- car/pages/index2/index2.js
+++ car/pages/index2/index2.js
@@ -1,71 +1,114 @@
 // pages/index2/index2.js.js
+import qqmap from '../../utils/map.js';
+import Sort from '../../utils/city_sort';   //城市排序
+let arr2 = [
+    { "id": "v1", "cityName": "北京" },
+    { "id": "v2", "cityName": "上海" },
+    { "id": "v5", "cityName": "天津" },
+    { "id": "v7", "cityName": "安徽" },
+    { "id": "v3", "cityName": "呼和浩特" },
+    { "id": "v4", "cityName": "杭州" },
+    { "id": "v9", "cityName": "海南" },
+    { "id": "v8", "cityName": "张家口" }
+];
+let str = Sort.pySegSort(arr2);
 Page({
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        citylist: str,    //城市列表
+        locateCity: '',     //当前城市
+        cityState: false,   //是否显示城市弹框
+        tabIndex:0,
+    },
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        this.getAddress();
+    },
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  },
-  goChat(){
-      wx.navigateTo({
-          url: '/pages/chat/chat',
-      })
-  }
+    },
+    //到达聊天页面
+    goChat() {
+        wx.navigateTo({
+            url: '/pages/chat/chat',
+        })
+    },
+    changeTab(e){
+        const { index } = e.currentTarget.dataset;
+        this.setData({
+            tabIndex:index
+        })
+    },
+    //初始化定位
+    getAddress() {
+        let cityOrTime = wx.getStorageSync('locatecity') || {},
+            time = new Date().getTime(),
+            city = '';
+        console.log(cityOrTime)
+        if (!cityOrTime.time || (time - cityOrTime.time > 1800000)) {//每隔30分钟请求一次定位
+            this.getLocate();
+        } else {//如果未满30分钟，那么直接从本地缓存里取值
+            this.setData({
+                locateCity: cityOrTime.city
+            })
+            console.log(this.data.locateCity)
+        }
+    },
+    //调用定位
+    getLocate() {
+        new qqmap().getLocateInfo().then(val => {//这个方法在另一个文件里，下面有贴出代码
+            if (val.indexOf('市') !== -1) {//这里是去掉“市”这个字
+                val = val.slice(0, val.indexOf('市'));
+            }
+            this.setData({
+                locateCity: val
+            });
+            //把获取的定位和获取的时间放到本地存储
+            wx.setStorageSync('locatecity', { city: val, time: new Date().getTime() });
+        });
+    },
+    //显示地址弹框
+    showCityHandle() {
+        this.setData({
+            cityState: true
+        })
+    },
+    //选择地址
+    cityTap(e) {
+        console.log(e);
+        const cityName = e.detail.cityname;
+        this.setData({
+            locateCity: cityName,
+            cityState: false
+        })
+        wx.setStorageSync('locatecity', { city: cityName, time: new Date().getTime() });
+    },
+    //关闭地址选择
+    closeCityHandle() {
+        this.setData({
+            cityState: false
+        })
+    },
 })
